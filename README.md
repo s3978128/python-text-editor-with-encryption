994c248eb3109aa16193b1bec20ea8788917a4eb
comet-candy <116716746+s3978128@users.noreply.github.com>
2023-11-18 04:12:16 +0700

    Added RSA encryption and testing file for convenience. Updated README.md

--- README.md
+++ README.md
@@ -1,32 +1,41 @@
-# Simple Text Editor
+# Secure Text Editor
 
-A simple text editor application developed using Python's Tkinter library.
+The Secure Text Editor is an application built using Python's Tkinter library, offering encryption functionalities for text files using `pyDes` and `pycryptodome`.
 
 ## Features:
-- Open File: Open and edit text files (supports .txt and all file types).
-- Save As: Save the current content to a file.
-- About: Display information about the application.
-- Clear: Clear the text editor content.
-- Dark Mode: Toggle between light and dark themes.
-- Counters: Automatically counts the number of lines, words, and characters in the text.
 
-## How to Run:
+### Encryption Options:
+- **DES Encryption:** Encrypt files using the symmetric DES (Data Encryption Standard) algorithm.
+- **RSA Encryption:** Employ asymmetric RSA (Rivest-Shamir-Adleman) encryption for secure file handling.
 
-### Requirements
-- Python 3.x installed
+### Text Editing:
+- **Open File:** Access and edit text files (supports .txt and various file formats).
+- **Save As:** Save the current content to a chosen file.
+- **Clear:** Empty the text editor's content.
+- **Dark Mode:** Switch between light and dark themes for better readability.
+- **About**: Provides information about the application.
+- **Counters**: Dynamically counts the number of lines, words, and characters in the text being edited.
 
-### Installation
+## How to Use:
+
+### Requirements:
+- Ensure Python 3.x is installed on your system.
+
+### Installation:
 1. Clone or download the repository to your local machine.
-2. Install the required Python packages if needed (`tkinter`).
+2. If necessary, install the required Python packages (`tkinter`, `pyDes`, `pycryptodome`).
 
-### Running the Application
-1. Open the terminal.
-2. Navigate to the directory where the `text_editor.py` file is located.
-3. Run the following command:
+### Running the Application:
+1. Open a terminal.
+2. Navigate to the directory where the `text_editor.py` file is.
+3. Execute the following command:
     ```
     python text_editor.py
     ```
-4. The application window will appear, allowing you to perform various text editing operations.
+4. The application window will appear, enabling you to perform text editing operations with encryption capabilities (DES or RSA encryption available). You can start by encrypting the provided `testingtext.txt` file, or create a new file.
+5. To use the DES encryption, you need to input a non-empty password. Note that due to limitations, passwords longer than 8 characters will be truncated.
+6. To use the RSA encryption, a pair of public and private keys is needed. Use the Generate Key button to generate the keys. You can then start using this encryption method.
 
-## Author: Nguyen Viet Ha (s3978128)
-## Course: Security for Computing and IT
+## Information:
+- **Author:** Nguyen Viet Ha (s3978128)
+- **Course:** Security for Computing and IT
--- text_editor.py
+++ text_editor.py
@@ -1,7 +1,13 @@
 import tkinter as tk
 from tkinter import messagebox, scrolledtext, Menu, filedialog, simpledialog
-import pyDes 
+import pyDes
+from Crypto.Cipher import PKCS1_OAEP
+from Crypto.PublicKey import RSA
+from Crypto.Random import get_random_bytes
+from Crypto import Random
+from Crypto.Hash import SHA256
 
+# Text editor functions
 def open_file():
     """Open a file for editing."""
     filepath = tk.filedialog.askopenfilename(
@@ -31,7 +37,7 @@ def save_file():
 
 def about_info():
     """Display information about the application."""
-    messagebox.showinfo("About", "A Simple Text Editor with Encryption Algorithms!\nNguyen Viet Ha s3978128\nSecurity for Computing and IT")
+    messagebox.showinfo("About", "A Simple Text Editor with Encryption Algorithms!\nNguyen Viet Ha s3978128\nSecurity for Computing and IT\nEncrypt your .txt files using symmetric (DES) or asymmetric (RSA) encryption algorithms.")
 
 def clear_text():
     """Clear the text editor."""
@@ -55,9 +61,21 @@ def update_counts(event=None):
     lbl_line_count.config(text=f"Lines: {lines}")
     lbl_word_count.config(text=f"Words: {words}")
     lbl_char_count.config(text=f"Characters: {characters}")
-    
-# Use DES algorithm to encrypt text files
-def encrypt_file(filepath, password):
+
+# Helper display function
+def display_decrypted_content(filepath):
+    """ 
+    Display decrypted content in text editor when decryption is complete.
+    """
+    txt_edit.delete("1.0", tk.END)
+    with open(filepath, mode="r", encoding="utf-8") as input_file:
+        text = input_file.read()
+        txt_edit.insert(tk.END, text)
+    window.title(f"Text Editor - {filepath}")
+
+
+# DES encryption and decryption functions
+def encrypt_file_des(filepath, password):
     """ 
     Encrypt a file using DES algorithm.
     Return the encrypted file path if successful.
@@ -78,7 +96,7 @@ def encrypt_file(filepath, password):
         
         # Remove ".txt" and add "-encrypted.txt"
         encrypted_filepath = filepath[:-4]
-        encrypted_filepath = encrypted_filepath + "-encrypted.txt"  # Change the file extension
+        encrypted_filepath = encrypted_filepath + "-des-encrypted.txt"  # Change the file extension
         
         # Write the encrypted data to the file
         with open(encrypted_filepath, 'wb') as file:
@@ -86,9 +104,9 @@ def encrypt_file(filepath, password):
         
         return encrypted_filepath
     except Exception as e:
-        messagebox.showerror("Error", f"Encryption failed: {e}")
+        messagebox.showerror("Error", f"Encryption failed. Possible error: {e}")
 
-def decrypt_file(filepath, password):
+def decrypt_file_des(filepath, password):
     """
     Decrypt a file using DES algorithm. 
     Return the decrypted file path if successful.
@@ -106,7 +124,7 @@ def decrypt_file(filepath, password):
         
         # Remove ".txt" and add "-decrypted.txt"
         decrypted_filepath = filepath[:-4]
-        decrypted_filepath = decrypted_filepath + "-decrypted.txt"
+        decrypted_filepath = decrypted_filepath + "-des-decrypted.txt"
         
         # Write the decrypted data to the file
         with open(decrypted_filepath, 'wb') as file:
@@ -114,10 +132,10 @@ def decrypt_file(filepath, password):
         
         return decrypted_filepath
     except Exception as e:
-        messagebox.showerror("Error", f"Decryption failed: {e}")
+        messagebox.showerror("Error", f"Decryption failed, check your password!")
 
 # Encryption and decryption functions called by the buttons
-def encrypt_file_with_password():
+def encrypt_file_des_with_password():
     """ 
     Encrypt a file using DES algorithm when the user clicks the "Encrypt File" button.
     """
@@ -139,14 +157,14 @@ def encrypt_file_with_password():
         messagebox.showerror("Error", "Password must not be empty!")
         return
 
-    # Encrypt the file using DES algorithm calling the encrypt_file function
-    encrypted_filepath = encrypt_file(filepath, password)
+    # Encrypt the file using DES algorithm calling the encrypt_file_des function
+    encrypted_filepath = encrypt_file_des(filepath, password)
     
     # If the encryption is successful
     if encrypted_filepath:
         messagebox.showinfo("Encryption Complete", f"File encrypted as {encrypted_filepath}")
 
-def decrypt_file_with_password():
+def decrypt_file_des_with_password():
     """ 
     Decrypt a file using DES algorithm when the user clicks the "Decrypt File" button.
     """
@@ -168,38 +186,143 @@ def decrypt_file_with_password():
         messagebox.showerror("Error", "Password must not be empty!")
         return
 
-    # Decrypt the file using DES algorithm calling the decrypt_file function
-    decrypted_filepath = decrypt_file(filepath, password)
+    # Decrypt the file using DES algorithm calling the decrypt_file_des function
+    decrypted_filepath = decrypt_file_des(filepath, password)
     
     # If the decryption is successful
     if decrypted_filepath:
         messagebox.showinfo("Decryption Complete", f"File decrypted as {decrypted_filepath}")
         display_decrypted_content(decrypted_filepath)  # Display decrypted content in text editor
 
-def display_decrypted_content(filepath):
-    """ 
-    Display decrypted content in text editor when decryption is complete.
+# RSA encryption and decryption functions
+# File paths for saving keys
+PUBLIC_KEY_FILE = "public_key.pem"
+PRIVATE_KEY_FILE = "private_key.pem"
+
+# Function to generate RSA keys and save them to files
+def generate_keys_and_save(keysize=2048):
+    """
+    Generate RSA public and private keys and save them to files.
+    Default key size is 2048 bits.
     """
     try:
-        # Read the decrypted content
-        with open(filepath, 'r', encoding='utf-8') as file:
-            text = file.read()
+        random_generator = Random.new().read
+        key = RSA.generate(keysize, random_generator)
+        public_key = key.publickey().export_key()
+        private_key = key.export_key()
         
-        # Clear the text editor and insert the decrypted content
-        txt_edit.delete('1.0', tk.END)
-        txt_edit.insert(tk.END, text)
+        # Save public key to file
+        with open(PUBLIC_KEY_FILE, 'wb') as pub_key_file:
+            pub_key_file.write(public_key)
         
-        # Update the window title
-        window.title(f"Text Editor - {filepath}")
+        # Save private key to file
+        with open(PRIVATE_KEY_FILE, 'wb') as priv_key_file:
+            priv_key_file.write(private_key)
+            messagebox.showinfo("Key Generation Complete", f"Public and private keys saved to {PUBLIC_KEY_FILE} and {PRIVATE_KEY_FILE}")
+        return public_key, private_key
     except Exception as e:
-        messagebox.showerror("Error", f"Failed to display decrypted content: {e}")
+        messagebox.showerror("Error", f"Key generation and saving failed: {e}")
+
+# Function to load existing RSA keys from files
+def load_keys():
+    """
+    Load RSA keys from saved files.
+    """
+    try:
+        with open(PUBLIC_KEY_FILE, 'rb') as pub_key_file:
+            public_key = pub_key_file.read()
         
+        with open(PRIVATE_KEY_FILE, 'rb') as priv_key_file:
+            private_key = priv_key_file.read()
+        
+        return public_key, private_key
+    except FileNotFoundError:
+        messagebox.showwarning("Warning", "Keys not found. Please generate new keys.")
+        return None, None
+    except Exception as e:
+        messagebox.showerror("Error", f"Key loading failed: {e}")
+
+def encrypt_file_rsa(filepath, public_key):
+    """ 
+    Encrypt a file using RSA public key.
+    Return the encrypted file path if successful.
+    """
+    try:
+        with open(filepath, 'rb') as file:
+            data = file.read()
+        
+        recipient_key = RSA.import_key(public_key)
+        cipher_rsa = PKCS1_OAEP.new(recipient_key)
+        encrypted_data = cipher_rsa.encrypt(data)
+        
+        encrypted_filepath = filepath[:-4] + "-rsa-encrypted.txt"
+        with open(encrypted_filepath, 'wb') as file:
+            file.write(encrypted_data)
+        return encrypted_filepath
+    except Exception as e:
+        messagebox.showerror("Encryption Failed", f"Error: {e}")
+
+def decrypt_file_rsa(filepath, private_key):
+    """ 
+    Decrypt a file using RSA private key.
+    Return the decrypted file path if successful.
+    """
+    try:
+        with open(filepath, 'rb') as file:
+            encrypted_data = file.read()
+        
+        key = RSA.import_key(private_key)
+        cipher_rsa = PKCS1_OAEP.new(key)
+        decrypted_data = cipher_rsa.decrypt(encrypted_data)
+        
+        decrypted_filepath = filepath[:-4] + "-rsa-decrypted.txt"
+        with open(decrypted_filepath, 'wb') as file:
+            file.write(decrypted_data)
+        return decrypted_filepath
+    except Exception as e:
+        messagebox.showerror("Decryption Failed", f"Error: {e}")
+        
+# Encryption and decryption functions called by the buttons
+def encrypt_file_rsa_with_loaded_key():
+    """
+    Encrypt a file using loaded RSA public key when the user clicks the "Encrypt File (RSA)" button.
+    """
+    filepath = tk.filedialog.askopenfilename(filetypes=[("Text Files", "*.txt"), ("All Files", "*.*")])
+    if not filepath:
+        return
+
+    public_key, _ = load_keys()  # Assuming load_keys() returns the loaded public and private keys
+    if public_key is None:
+        return
+
+    encrypted_filepath = encrypt_file_rsa(filepath, public_key)
+    if encrypted_filepath:
+        messagebox.showinfo("Encryption Complete", f"File encrypted as {encrypted_filepath}")
+
+def decrypt_file_rsa_with_loaded_key():
+    """ 
+    Decrypt a file using loaded RSA private key when the user clicks the "Decrypt File (RSA)" button.
+    """
+    filepath = tk.filedialog.askopenfilename(filetypes=[("Text Files", "*.txt"), ("All Files", "*.*")])
+    if not filepath:
+        return
+    
+    _, private_key = load_keys()  # Assuming load_keys() returns the loaded public and private keys
+    if private_key is None:
+        return
+    
+    decrypted_filepath = decrypt_file_rsa(filepath, private_key)
+    if decrypted_filepath:
+        display_decrypted_content(decrypted_filepath)  # Display decrypted content in text editor
+        messagebox.showinfo("Decryption Complete", f"File decrypted as {decrypted_filepath}")
+
+
 # Create the main window
 window = tk.Tk()
 window.title("Text Editor")
 
 # Set window size and layout
-window.geometry("1000x550")  # Set window size
+window.geometry("980x580")  # Set window size
 window.resizable(False, False)  # Set resizable to False for both x and y directions
 
 # Create Text widget with scrollbar
@@ -208,11 +331,16 @@ txt_edit = scrolledtext.ScrolledText(window, wrap=tk.WORD, font=("Arial", 12))
 # Create Buttons
 btn_open = tk.Button(window, text="Open", width=10, command=open_file)
 btn_save = tk.Button(window, text="Save As...", width=10, command=save_file)
-btn_about = tk.Button(window, text="About", width=10, command=about_info)
+btn_dark_mode = tk.Button(window, text="Toggle Dark", width=10, command=toggle_dark_mode)
 btn_clear = tk.Button(window, text="Clear", width=10, command=clear_text)
-btn_dark_mode = tk.Button(window, text="Toggle Dark", width=15, height=2, command=toggle_dark_mode)
-btn_encrypt = tk.Button(window, text="Encrypt File", width=15, height=2, bg="blue", fg="white", command=encrypt_file_with_password)
-btn_decrypt = tk.Button(window, text="Decrypt File", width=15, height=2, bg="green", fg="white", command=decrypt_file_with_password)
+
+btn_about = tk.Button(window, text="About", width=25, height=2, command=about_info)
+btn_des_encrypt = tk.Button(window, text="Encrypt from Files... (DES)", width=25, height=2, bg="lightblue", fg="black", command=encrypt_file_des_with_password)
+btn_des_decrypt = tk.Button(window, text="Decrypt from Files... (DES)", width=25, height=2, bg="lightblue", fg="black", command=decrypt_file_des_with_password)
+
+btn_generate_keys = tk.Button(window, text="Generate Keys (RSA)", width=25, height=2, bg="lightpink", fg="black",command=generate_keys_and_save)
+btn_rsa_encrypt = tk.Button(window, text="Encrypt from Files... (RSA)", width=25, height=2, bg="lightpink", fg="black", command=encrypt_file_rsa_with_loaded_key)
+btn_rsa_decrypt = tk.Button(window, text="Decrypt from Files... (RSA)", width=25, height=2, bg="lightpink", fg="black", command=decrypt_file_rsa_with_loaded_key)
 
 # Create Menubar
 menubar = Menu(window)
@@ -229,18 +357,29 @@ lbl_line_count = tk.Label(window, text="Lines: 0")
 lbl_word_count = tk.Label(window, text="Words: 0")
 lbl_char_count = tk.Label(window, text="Characters: 0")
 
+# Label for key warning
+lbl_key_warning = tk.Label(window, text="Save the file before encrypting.\nGenerate keys before RSA encryption if you haven't!", fg="red")
+
 # Place widgets in the window using grid layout
 txt_edit.grid(row=0, column=0, rowspan=6, columnspan=4, padx=10, pady=10)
 btn_open.grid(row=6, column=0, padx=5, pady=5)
 btn_save.grid(row=6, column=1, padx=5, pady=5)
-btn_about.grid(row=6, column=2, padx=5, pady=5)
+btn_dark_mode.grid(row=6, column=2, padx=5, pady=5)
 btn_clear.grid(row=6, column=3, padx=5, pady=5)
-btn_dark_mode.grid(row=0, column=4, padx=5, pady=5)
-btn_encrypt.grid(row=1, column=4, padx=5, pady=5)
-btn_decrypt.grid(row=2, column=4, padx=5, pady=5)
+btn_about.grid(row=0, column=4, padx=5, pady=5)
+
+btn_des_encrypt.grid(row=1, column=4, padx=5, pady=5)
+btn_des_decrypt.grid(row=2, column=4, padx=5, pady=5)
+
+btn_generate_keys.grid(row=3, column=4, padx=5, pady=5)
+btn_rsa_encrypt.grid(row=4, column=4, padx=5, pady=5)
+btn_rsa_decrypt.grid(row=5, column=4, padx=5, pady=5)
+
 lbl_line_count.grid(row=7, column=0, padx=5, pady=5)
 lbl_word_count.grid(row=7, column=1, padx=5, pady=5)
 lbl_char_count.grid(row=7, column=2, padx=5, pady=5)
+
+lbl_key_warning.grid(row=8, column=0, columnspan=4, padx=5, pady=5)
 
 # Bind events to text editing (e.g., typing, deleting) to update counts dynamically
 txt_edit.bind('<Key>', update_counts)
